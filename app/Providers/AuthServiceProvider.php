9f0e912ac7fab5e3c0d415b95c67d545aecdea9e
gezek <ervasevim@gmail.com>
2025-07-31 01:55:02 +0300

    Initial commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+    }
+}
